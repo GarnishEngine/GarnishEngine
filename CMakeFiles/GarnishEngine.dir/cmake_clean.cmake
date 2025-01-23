652f4862fb8bd838e1e069a974038085bcc71bf2
Kevin Huai <142623053+KevinH15291@users.noreply.github.com>
2025-01-22 20:40:17 -0700

    BREAKING CHANGE!: Hello World

---
+++ CMakeFiles/GarnishEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GarnishEngine.dir/main.cc.o"
+  "CMakeFiles/GarnishEngine.dir/main.cc.o.d"
+  "GarnishEngine"
+  "GarnishEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GarnishEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/GarnishEngine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for GarnishEngine.
+# This may be replaced when dependencies are built.
