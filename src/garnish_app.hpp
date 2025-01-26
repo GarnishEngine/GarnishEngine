a8b1788f631c92518b4a2a5a3430af0bac939b43
Kevin Huai <142623053+KevinH15291@users.noreply.github.com>
2025-01-26 11:42:57 -0700

    feat: Dynamic linking should work?

--- src/garnish_app.hpp
+++ src/garnish_app.hpp
@@ -3,7 +3,7 @@
 #include "SDL3/SDL_events.h"
 #include "Rendering/OpenGL/gl_buffer.hpp"
 #include "Rendering/OpenGL/shader_program.hpp"
-#include "Rendering/camera.hpp"
+#include "Utility/camera.hpp"
 #include "garnish_entity.hpp"
 #include "garnish_event.hpp"
 #include "garnish_window.hpp"
