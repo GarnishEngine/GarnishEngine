867d67a745b3edf9174d262b4c0af0228086d764
Kevin Huai <142623053+KevinH15291@users.noreply.github.com>
2025-07-05 18:43:09 -0600

    fix: made sanitizers target link, not just link

--- examples/Pong/pong.cc
+++ examples/Pong/pong.cc
@@ -1,33 +1,35 @@
 #include "pong.h"
 
+#include <cstdint>
+
+#include "shader_program.hpp"
+#include "vulkan_renderer.hpp"
 
 int main() {
-    garnish::App app{ };
+    garnish::App app{
+        {.backend = RenderingBackend::Vulkan,
+         .width = 800,
+         .height = 600,
+         .targetFps = 144}
+    };
+    app.get_controller().register_component<Renderable>();
 
-    auto i = app.ecsController.register_system<ImGuiSystem>(0);
-    auto c = app.ecsController.register_system<CameraSystem>(0);
-    auto m = app.ecsController.register_system<MeshSystem>(0);
+    uint32_t meshHandle =
+        app.get_render_device()->setup_mesh("Models/viking_room.obj");
 
-    app.ecsController.register_component<Camera>();
-    app.ecsController.register_component<Mesh>();
-    app.ecsController.register_component<Sprite>();
+    uint32_t texHandle =
+        app.get_render_device()->load_texture("Textures/viking_room.png");
 
-    auto camera_entity = app.ecsController.create_entity_with_components(Camera());
+    auto vikingRoom = app.get_controller().create_entity_with_components(
+        Renderable{.meshHandle = meshHandle, .texHandle = texHandle}
+    );
 
+    // // TODO 3d mesh
+    //  = app.get_controller().create_entity();
 
-    auto vikingRoom = app.ecsController.create_entity();
-
-    garnish::Mesh gMesh;
-    garnish::Texture texture;
-    texture.load_texture("Textures/viking_room.png");
-
-    gMesh.load_mesh("Models/Untitled.obj");
-    gMesh.setup_mesh();
-    gMesh.load_texture(texture);
-
-    app.ecsController.add_component(vikingRoom, gMesh);
-
-
+    // garnish::Mesh gMesh;
+    // garnish::Texture texture;
+    // texture.load_texture();
 
     app.run();
 }
