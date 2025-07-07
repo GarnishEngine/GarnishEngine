cb80ada08d0e5809a29841141ed5d1c860bb954a
Benjamin Bingham <ben@benbingham.ca>
2025-07-07 18:10:12 -0400

    Simplified TestApp

--- examples/TestApp/test_app.cc
+++ examples/TestApp/test_app.cc
@@ -6,7 +6,6 @@
 int main() {
     garnish::App app{};
 
-    // auto i = app.get_controller().register_system<ImGuiSystem>(0);
     auto c = app.get_controller().register_system<CameraSystem>(0);
 
     app.get_controller().register_component<Camera>();
@@ -19,8 +18,6 @@ int main() {
         app.get_render_device()->setup_mesh("Models/viking_room.obj");
     auto tex =
         app.get_render_device()->load_texture("Textures/viking_room.png");
-
-    app.get_controller().create_entity_with_components(Camera());
 
     auto vikingRoom = app.get_controller().create_entity_with_components(
         Renderable{.meshHandle = meshInstance, .texHandle = tex}
--- examples/TestApp/test_app.h
+++ examples/TestApp/test_app.h
@@ -20,43 +20,6 @@ static float pitch = 0;
 static float x = 0;
 static float y = 0;
 
-// class ImGuiSystem : public System {
-//    public:
-//     void update(ECSController& world) override {
-//         ImGui::ShowDemoWindow();
-
-//         static float f = 0.0f;
-//         static int counter = 0;
-
-//         ImGui::Begin("Hello, world!");  // Create a window called "Hello,
-//                                         // world!" and append into it.
-
-//         ImGui::Text("This is some useful text.");  // Display some text (you
-//         can
-//                                                    // use a format strings
-//                                                    too)
-
-//         if (ImGui::Button(
-//                 "Button"
-//             ))  // Buttons return true when clicked (most widgets
-//                 // return true when edited/activated)
-//             counter++;
-//         ImGui::SameLine();
-//         ImGui::Text("counter = %d", counter);
-//         ImGui::Text("pitch = %f", pitch);
-//         ImGui::Text("yaw = %f", yaw);
-//         ImGui::Text("x = %f", x);
-//         ImGui::Text("y = %f", y);
-
-//         ImGuiIO& io = ImGui::GetIO();
-//         ImGui::Text(
-//             "Application average %.3f ms/frame (%.1f FPS)",
-//             1000.0f / io.Framerate,
-//             io.Framerate
-//         );
-//         ImGui::End();
-//     }
-// };
 class CameraSystem : public System {
    public:
     int32_t WIDTH = 600;
